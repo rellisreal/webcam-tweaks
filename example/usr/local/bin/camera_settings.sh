a9609cfc1af5a23bb17cf3a553bc8a7cde8c8e29
rellisreal <gendarkdragon@gmail.com>
2025-07-06 16:40:47 +0800

    Updated example sub-folder

--- example/usr/local/bin/camera_settings.sh
+++ example/usr/local/bin/camera_settings.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Script made, to automate the painful process of automating the brightness/exposure settings of my Logitech HD Pro Webcam C920.
@@ -11,11 +10,14 @@ if [ $HOUR -ge 6 ] && [ $HOUR -le 18 ]; then
     v4l2-ctl --set-ctrl=auto_exposure=1
     v4l2-ctl --set-ctrl=exposure_time_absolute=300
     v4l2-ctl --set-ctrl=brightness=255
+    v4l2-ctl --set-ctrl=gain=50
+    v4l2-ctl --set-ctrl=contrast=150
     echo "Daytime settings applied!!!"
 else 
     v4l2-ctl --set-ctrl=brightness=255
     v4l2-ctl --set-ctrl=auto_exposure=3
     v4l2-ctl --set-ctrl=exposure_time_absolute=156
+    v4l2-ctl --set-ctrl=gain=0
+    v4l2-ctl --set-ctrl=contrast=128
     echo "Nighttime settings applied!!!"
 fi 
-
